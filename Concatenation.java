c047abec1c153d6eb77c25f501313faf6435dbb2
8RAPS8 <8rahulpatel8@gmail.com>
2021-07-09 16:42:33 +0530

    my

---
+++ Concatenation.java
@@ -0,0 +1,14 @@
+package in.co.rays1;
+
+public class Concatenation {
+
+	public static void main(String[] args) {
+		String firstName="Vijay";
+		String lastName="Chauhan";
+		System.out.println("Concatenation:"+firstName.concat(lastName));
+		String firstName1="Rahul";
+		String lastName1="Patel";
+		System.out.println("Concatenation:"+firstName1.concat(lastName1));
+	}
+
+}
