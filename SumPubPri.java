9b670791c0e8cefdf8ebb3734cb89526a3d5df19
8RAPS8 <8rahulpatel8@gmail.com>
2021-07-12 18:04:03 +0530

    July12

---
+++ SumPubPri.java
@@ -0,0 +1,22 @@
+package in.co.rays1;
+
+public class SumPubPri {
+
+	public static void main(String[] args) {
+		
+		System.out.println(multiply(20,30,40));			
+		}
+	
+	private static int sum(int a, int b) {
+		
+		return (a+b);
+		}
+	
+	private static int multiply(int a, int b,int c) {
+		return sum(a,b)*c;
+		}
+	
+	}
+
+
+
